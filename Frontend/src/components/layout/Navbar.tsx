56ba9e5f6268d004486bca288c7f02bc8f12bba4
Matpech <315011607+Matpech@users.noreply.github.com>
2026-09-05 15:06:30 +0200

    Added responsive to Navbar component
    [o] Mobile version shows icons instead of text

--- Frontend/src/components/layout/Navbar.tsx
+++ Frontend/src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Wallet } from "lucide-react"
+import { FileText, LogOut, User, UserCog, Wallet } from "lucide-react"
 import { Link } from "react-router-dom"
 import { useLocalizedPath } from "../../hooks/useLocalizedPath"
 import { useContext } from "react"
@@ -14,34 +14,52 @@ function Navbar() {
 
     return (
         <header className="
-            flex justify-between items-center
-            border-x-2 border-b-2 p-2 border-yellow-600
+            flex justify-between items-center min-h-20
+            md:border-x-2 border-b-2 p-2 border-yellow-600
             bg-gray-50 dark:bg-zinc-900
             dark:text-white
         ">
             <div className="flex items-center gap-2">
-                <Wallet size={64} color="var(--color-yellow-600)" />
+                <Wallet size={64} color="var(--color-yellow-600)" className="hidden md:block" />
                 <div>
                     <Link to={toLocalized('/dashboard')}>
                         <h1 className="text-3xl font-bold">FoxBudg</h1>
                     </Link>
-                    <p className="italic">{t('header.slogan')}</p>
+                    <p className="italic hidden md:block">{t('header.slogan')}</p>
                 </div>
             </div>
 
+            {/* Navigation links (icons on mobile, text on desktop) */}
             <nav className="flex gap-4 text-xl">
-                <Link to={toLocalized(`/profile/${user?.id}`)}>{t('header.navigation.myProfile')}</Link>
+                {/* Link to user's profile */}
+                <Link to={toLocalized(`/profile/${user?.id}`)}>
+                    <span className="hidden md:block">{t('header.navigation.myProfile')}</span>
+                    <User size={32} className="md:hidden" />
+                </Link>
+                
+                {/* Link to expense reports manager (accessible by accountants and managers) */}
                 {user && ["accountant", "manager"].includes(user.role) && (
-                    <Link to={toLocalized('/reports')}>{t('header.navigation.reportsPage')}</Link>
+                    <Link to={toLocalized('/reports')}>
+                        <span className="hidden md:block">{t('header.navigation.reportsPage')}</span>
+                        <FileText size={32} className="md:hidden" />
+                    </Link>
                 )}
+
+                {/* Link to user management page (accessible by managers only) */}
                 {user && user.role === "manager" && (
-                    <Link to={toLocalized('/admin')}>{t('header.navigation.adminPage')}</Link>
+                    <Link to={toLocalized('/admin')}>
+                        <span className="hidden md:block">{t('header.navigation.adminPage')}</span>
+                        <UserCog size={32} className="md:hidden" />
+                    </Link>
                 )}
+
+                {/* Logout button */}
                 <button
                     className="cursor-pointer"
                     onClick={async () => await logout()}
                 >
-                    {t('header.navigation.logout')}
+                    <span className="hidden md:block">{t('header.navigation.logout')}</span>
+                    <LogOut size={32} className="md:hidden" />
                 </button>
             </nav>
         </header>
