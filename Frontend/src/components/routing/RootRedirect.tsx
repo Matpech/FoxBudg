78a89a212f1e810fb7c1dddb1967ae21f5313292
Matpech <315011607+Matpech@users.noreply.github.com>
2026-09-05 12:48:05 +0200

    Fixed a bug with RootRedirect
    [o] RootRedirect now checks if the lang parameter exists in the URL before using the browser language for redirection

--- Frontend/src/components/routing/RootRedirect.tsx
+++ Frontend/src/components/routing/RootRedirect.tsx
@@ -1,14 +1,24 @@
-import { Navigate } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 import { DEFAULT_LANG, SUPPORTED_LANGS } from "../../i18n/config"
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/AuthContext"
 
 /**
- * When the user accesses the application without specifying a language, redirect to a specific language using browser preferences
+ * When the user accesses the application without specifying a language, redirect to a specific language using browser preferences.
+ * 
+ * This component can also get triggered if the user requests /:lang without anything after.
+ * Therefore, we check if the lang parameter exists before using the browser language
  */
 export default function RootRedirect() {
-    const browserLanguage = navigator.language.slice(0, 2)
-    const lang = SUPPORTED_LANGS.includes(browserLanguage) ? browserLanguage : DEFAULT_LANG
+    const params = useParams()
+    let lang = DEFAULT_LANG
+    if (params.lang) {
+        lang = params.lang
+    } else {
+        const browserLanguage = navigator.language.slice(0, 2)
+        lang = SUPPORTED_LANGS.includes(browserLanguage) ? browserLanguage : DEFAULT_LANG
+    }
+
 
     // Redirect the user depending on auth status
     // -> dashboard if authenticated
