d634d23b1bda2fb1c0cb6af5ea906924110c7fca
Matpech <315011607+Matpech@users.noreply.github.com>
2026-09-12 13:46:14 +0200

    Added actions column in the users table and adjusted table responsiveness
    [+] Created an ActionMenu component to group multiple actions in a context menu
    [o] Applied the ActionMenu to the users table
    [o] Updated localization files

--- Frontend/src/components/UsersTable.tsx
+++ Frontend/src/components/UsersTable.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from "react-i18next"
 import type { User } from "../types/users"
+import ActionMenu from "./ui/ActionMenu"
+import GenericButton from "./ui/GenericButton"
 
 interface Props {
     users: User[] | null
@@ -27,9 +29,10 @@ function UsersTable({ users }: Props) {
                     <table className="w-full table-fixed not-md:text-sm">
                         <thead className="text-left font-semibold">
                             <tr className="border-b border-yellow-600">
-                                <th className="px-2 py-1 md:w-6/12">{t('userManager.table.header.name')}</th>
-                                <th className="px-2 py-1 md:w-4/12">{t('userManager.table.header.email')}</th>
-                                <th className="px-2 py-1 md:w-2/12">{t('userManager.table.header.role')}</th>
+                                <th className="px-2 py-1">{t('userManager.table.header.name')}</th>
+                                <th className="px-2 py-1">{t('userManager.table.header.email')}</th>
+                                <th className="px-2 py-1">{t('userManager.table.header.role')}</th>
+                                <th className="px-2 py-1 w-16">{t('userManager.table.header.actions')}</th>
                             </tr>
                         </thead>
 
@@ -39,9 +42,29 @@ function UsersTable({ users }: Props) {
                                     key={user.id}
                                     className="even:bg-gray-200 dark:even:bg-zinc-900 cursor-pointer"
                                 >
-                                    <td className="px-2 not-md:py-1">{user.first_name} {user.last_name.toUpperCase()}</td>
-                                    <td className="px-2 not-md:py-1">{user.email}</td>
+                                    <td className="px-2 not-md:py-1 wrap-break-word">{user.first_name} {user.last_name.toUpperCase()}</td>
+                                    <td className="px-2 not-md:py-1 wrap-break-word">{user.email}</td>
                                     <td className="px-2 not-md:py-1">{t(`users.roles.${user.role}`, { ns: 'common' })}</td>
+                                    <td className="not-md:py-1 flex justify-end">
+                                        <ActionMenu>
+                                            <div className="flex flex-col gap-2">
+                                                <p className="dark:text-white font-semibold text-xl">{user.first_name} {user.last_name.toUpperCase()}</p>
+
+                                                <GenericButton
+                                                    click={() => {}}
+                                                >
+                                                    {t('userManager.actions.update')}
+                                                </GenericButton>
+
+                                                <GenericButton
+                                                    type="danger"
+                                                    click={() => {}}
+                                                >
+                                                    {t('userManager.actions.delete')}
+                                                </GenericButton>
+                                            </div>
+                                        </ActionMenu>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
